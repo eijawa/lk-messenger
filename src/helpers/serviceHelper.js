2a8a4c7d0d172270ed250151480b4e6ef002dc5a
int1m <maxmonsta@gmail.com>
2022-02-25 22:50:44 +0300

    feat(serviceHelper): fix

--- src/helpers/serviceHelper.js
+++ src/helpers/serviceHelper.js
@@ -1,16 +1,19 @@
-export const useFetch = async (url, params = { method: 'GET', body: null }) => {
+export const useFetch = async (url, params = null) => {
     const token = localStorage.getItem('jwt');
+    let paramsValue = null;
 
     if (token) {
-        const response = await fetch(url, {
-            method: params?.method,
-            body: params?.body,
-            ContentType: 'application/json',
-            'Access-Control-Allow-Origin': '*',
-            Authorization: `Bearer ${token}`,
-        });
-        const data = await response.json();
-        return data;
+        if (params === null) {
+            paramsValue = {
+                method: 'GET',
+                Authorization: `Bearer ${token}`,
+            };
+        } else {
+            paramsValue = params;
+        }
+
+        const response = await fetch(url, paramsValue);
+        return response.json();
     }
     window.location.href = 'https://lk.volsu.ru';
     return {};
