4282325de460ccd34a40d996b94b1237ac40f9b8
eijawa <evgeniy.therabbit@gmail.com>
2022-02-26 03:41:09 +0300

    feat: Unify chat component

--- src/components/ChatsListItem.vue
+++ src/components/ChatsListItem.vue
@@ -1,41 +1,32 @@
 <template>
-  <li
-    class="chat"
-    :class="{ 'opened': isOpened }"
+  <base-chat-item
+    render-tag="li"
+    :avatar-src="chat.avatarSrc"
+    :title="chat.title"
+    :text="chat.lastMessage?.text"
+    is-hovered
+    :is-focused="isOpened"
     @click="onClickHandler"
   >
-    <div class="chat__avatar">
-      <a-avatar
-        :src="chat.avatarSrc"
-        shape="circle"
-        :size="54"
-        style="background-color:var(--volsu-yellow); font-size: 1.15rem; font-weight: bold;"
-      >{{ titleFirstLetters }}</a-avatar>
-    </div>
 
-    <div class="chat__content">
-      <div class="chat__header">
-        <span class="chat__title typo" :title="chat.title">{{ chat.title }}</span>
+    <template #info>
+      <readed-label v-if="isYourMessage" :is-readed="chat.lastMessage?.isReaded"></readed-label>
+      <date-label :date="chat.lastMessage?.date"></date-label>
+    </template>
 
-        <div class="chat__info">
-          <readed-label v-if="isYourMessage" :is-readed="chat.lastMessage?.isReaded"></readed-label>
-          <date-label :date="chat.lastMessage?.date"></date-label>
-        </div>
-      </div>
+    <template #prefix>
+      <attachment
+        v-if="chat.lastMessage?.attachment != null"
+        :attachment="chat.lastMessage?.attachment"
+        is-minified
+      />
+    </template>
 
-      <div class="chat__msg">
-        <attachment
-          v-if="chat.lastMessage?.attachment != null"
-          :attachment="chat.lastMessage?.attachment"
-          is-minified
-        />
+    <template #suffix>
+      <a-tag v-if="chat.newMessagesCount != 0" class="chat__count">{{ chat.newMessagesCount }}</a-tag>
+    </template>
 
-        <span class="chat__text typo">{{ chat.lastMessage?.text }}</span>
-
-        <a-tag v-if="chat.newMessagesCount != 0" class="chat__count">{{ chat.newMessagesCount }}</a-tag>
-      </div>
-    </div>
-  </li>
+  </base-chat-item>
 </template>
 
 <script setup>
@@ -44,8 +35,7 @@ import { computed } from 'vue';
 import Attachment from '@/components/kit/Attachment.vue';
 import ReadedLabel from '@/components/kit/ReadedLabel.vue';
 import DateLabel from '@/components/kit/DateLabel.vue';
-
-import getFirstLetters from '@/helpers/firstLettersHelper';
+import BaseChatItem from '@/components/base/BaseChatItem.vue';
 
 const props = defineProps({
   chat: {
@@ -70,76 +60,12 @@ const isYourMessage = computed(() => true);
 const onClickHandler = () => {
   emits('open', props.convid);
 };
-
-// Avatar functions
-const titleFirstLetters = computed(() => getFirstLetters(props.chat.title));
 </script>
 
 <style lang="scss" scoped>
-.chat {
-  --avatar-size: 54px;
-  --column-gap: 0.5rem;
+.ant-tag {
+  margin-right: 0;
 
-  padding: 0.5rem;
-
-  display: flex;
-  place-items: center;
-  column-gap: var(--column-gap);
-
-  &__content {
-    flex: 1 1 auto;
-
-    overflow: hidden;
-  }
-
-  &__header,
-  &__msg {
-    display: flex;
-    column-gap: var(--column-gap);
-  }
-
-  &__title,
-  &__text {
-    flex-grow: 1;
-  }
-
-  &__title {
-    font-weight: bold;
-    color: var(--volsu-black);
-  }
-
-  &__msg {
-    color: var(--volsu-dark-gray);
-  }
-
-  &__info {
-    display: flex;
-    place-items: center;
-    column-gap: 0.25rem;
-
-    color: var(--volsu-dark-gray);
-  }
-
-  .ant-tag {
-    margin-right: 0;
-
-    border-radius: 50%;
-  }
-
-  &:not(.opened):hover {
-    background-color: var(--light-gray);
-
-    cursor: pointer;
-  }
-
-  &.opened {
-    background-color: var(--volsu-black);
-  }
-
-  &.opened &__title,
-  &.opened &__msg,
-  &.opened &__info {
-    color: white;
-  }
+  border-radius: 50%;
 }
 </style>
---
+++ src/components/base/BaseChatItem.vue
@@ -0,0 +1,145 @@
+<template>
+  <component
+    :is="renderTag"
+    class="chat"
+    :class="{ 'hovered': isHovered, 'focused': isFocused }"
+  >
+    <div class="chat__avatar">
+      <a-avatar
+        :src="avatarSrc"
+        shape="circle"
+        :size="avatarSize"
+        style="background-color:var(--volsu-yellow); font-size: 1.15rem; font-weight: bold;"
+      >{{ titleFirstLetters }}</a-avatar>
+    </div>
+
+    <div class="chat__content">
+      <div class="chat__header">
+        <span class="chat__title typo" :title="title">{{ title }}</span>
+
+        <div class="chat__info">
+          <slot name="info"></slot>
+        </div>
+      </div>
+
+      <div class="chat__footer">
+        <slot name="prefix"></slot>
+
+        <span class="chat__text typo">
+          <slot name="text">
+            {{ text }}
+          </slot>
+        </span>
+
+        <slot name="suffix"></slot>
+      </div>
+    </div>
+  </component>
+</template>
+
+<script setup>
+import { computed } from 'vue';
+
+import getFirstLetters from '@/helpers/firstLettersHelper';
+
+const props = defineProps({
+  title: {
+    type: String,
+    default: '',
+  },
+  avatarSrc: {
+    type: String,
+    default: '',
+  },
+  avatarSize: {
+    type: Number,
+    default: 54,
+  },
+  text: {
+    type: String,
+    default: '',
+  },
+  isHovered: {
+    type: Boolean,
+    default: true,
+  },
+  isFocused: {
+    type: Boolean,
+    default: false,
+  },
+  renderTag: {
+    type: String,
+    default: 'div',
+  },
+});
+
+// Avatar functions
+const titleFirstLetters = computed(() => getFirstLetters(props.title));
+</script>
+
+<style lang="scss" scoped>
+.chat {
+  --avatar-size: 54px;
+  --column-gap: 0.5rem;
+
+  padding: 0.5rem;
+
+  display: flex;
+  place-items: center;
+  column-gap: var(--column-gap);
+
+  &__content {
+    flex: 1 1 auto;
+
+    overflow: hidden;
+  }
+
+  &__header,
+  &__footer {
+    display: flex;
+    column-gap: var(--column-gap);
+  }
+
+  &__footer {
+    color: var(--volsu-dark-gray);
+  }
+
+  &__title,
+  &__text {
+    flex-grow: 1;
+  }
+
+  &__title {
+    font-weight: bold;
+    color: var(--volsu-black);
+  }
+
+  &__info {
+    display: flex;
+    place-items: center;
+    column-gap: 0.25rem;
+
+    color: var(--volsu-dark-gray);
+  }
+}
+
+.hovered {
+  &:not(.focused):hover {
+    background-color: var(--light-gray);
+
+    cursor: pointer;
+  }
+}
+
+.focused {
+  background-color: var(--volsu-black);
+
+  .chat {
+    &__title,
+    &__footer,
+    &__info {
+      color: white;
+    }
+  }
+}
+</style>
