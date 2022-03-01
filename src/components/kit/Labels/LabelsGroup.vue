9635301612e2b0c86538bd70e830325880d11ef4
SparkleDiscord <evgeniy.therabbit@gmail.com>
2022-03-01 20:57:22 +0300

    feat: Add Labels Group element

---
+++ src/components/kit/Labels/LabelsGroup.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="labels-group" :class="{ 'reverse': isReverse, 'background': isHaveBackground }">
+    <slot></slot>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  isHaveBackground: {
+    type: Boolean,
+    default: false,
+  },
+  isReverse: {
+    type: Boolean,
+    default: false,
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+  .labels-group {
+    padding: 0 0.365rem;
+
+    display: flex;
+    place-items: center;
+    column-gap: 0.125rem;
+
+    float: right;
+
+    &.background {
+      background: rgba(0, 0, 0, 0.2);
+    }
+
+    &.reverse {
+      flex-direction: row-reverse;
+    }
+  }
+</style>
