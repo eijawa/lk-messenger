68dca835dbbf362fa7f6636866331fbbb56117b4
SparkleDiscord <evgeniy.therabbit@gmail.com>
2022-03-01 22:01:31 +0300

    ref: Remove faCircleCheck icon

--- src/components/kit/Labels/ReadedLabel.vue
+++ src/components/kit/Labels/ReadedLabel.vue
@@ -1,5 +1,5 @@
 <template>
-      <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" style="fill: var(--light-green);transform: ;msFilter:;"><path d="m2.394 13.742 4.743 3.62 7.616-8.704-1.506-1.316-6.384 7.296-3.257-2.486zm19.359-5.084-1.506-1.316-6.369 7.279-.753-.602-1.25 1.562 2.247 1.798z"></path></svg>
+      <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" style="transform: ;msFilter:;"><path d="m2.394 13.742 4.743 3.62 7.616-8.704-1.506-1.316-6.384 7.296-3.257-2.486zm19.359-5.084-1.506-1.316-6.369 7.279-.753-.602-1.25 1.562 2.247 1.798z"></path></svg>
 </template>
 
 <script setup>
--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
 // TODO: Dynamic import from array of dict
 
-import { faBars, faCircleCheck, faFile, faFileImage, faFilePdf,
+import { faBars, faFile, faFileImage, faFilePdf,
   faFileWord, faFilePowerpoint, faFileExcel, faFileZipper, faFileLines,
   faArrowLeft, faMagnifyingGlass, faEllipsisVertical, faPaperclip,
   faPaperPlane, faPen, faUser, faUsers, faChevronLeft } from '@fortawesome/free-solid-svg-icons';
@@ -22,7 +22,7 @@ import router from './routes/router';
 FontAwesomeLibraryCore.add([faBars, faChevronLeft]);
 
 // Used in Chats and Messages
-FontAwesomeLibraryCore.add([faCircleCheck]);
+FontAwesomeLibraryCore.add([]);
 
 // Used in NewChatBtn
 FontAwesomeLibraryCore.add([faPen, faUser, faUsers]);
