29a091a56a5552b95ab41822a2a336e75c9dd7c8
eijawa <evgeniy.therabbit@gmail.com>
2022-03-02 00:50:03 +0300

    ref: Remove useless auto-import

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import path from 'path';
 import { defineConfig } from 'vite';
 import Vue from '@vitejs/plugin-vue';
 
-import AutoImport from 'unplugin-auto-import/vite';
 import Components from 'unplugin-vue-components/vite';
 import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers';
 
@@ -16,9 +15,6 @@ export default defineConfig({
   plugins: [
     Vue({
       include: [/\.vue$/],
-    }),
-    AutoImport({
-      resolvers: [AntDesignVueResolver()],
     }),
     Components({
       resolvers: [AntDesignVueResolver()],
