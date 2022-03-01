58f3794abb3bb3e89c0b8bcce708720b1913cb58
eijawa <evgeniy.therabbit@gmail.com>
2022-03-02 00:53:07 +0300

    ref: Clear chats store state, move mock generation in fetch action

--- src/stores/chatsStore.js
+++ src/stores/chatsStore.js
@@ -13,55 +13,6 @@ export const useChatsStore = defineStore('chatsStore', {
   state: () => {
     const chats = ref([]);
 
-    if (config.isUseMockedData) {
-
-      // eslint-disable-next-line arrow-body-style
-      const generateAvatar = () => {
-        return faker.random.arrayElement([faker.image.avatar(), '']);
-      };
-
-      // eslint-disable-next-line arrow-body-style
-      const generateUser = (index, avatar) => {
-        return {
-          id: String(index),
-          avatarSrc: avatar,
-          username: faker.name.firstName(),
-          university: faker.random.arrayElement(['eaf', 'pt', 'pht', 'ns', 'pac', 'wwe', 'care', 'hwwcast', 'mait', 'r']),
-          type: faker.random.arrayElement(['student', 'teacher']),
-        };
-      };
-
-      // eslint-disable-next-line arrow-body-style
-      chats.value = Array.from({ length: 12 }, (el, idx) => {
-        const chatType = faker.random.arrayElement(['tat', 'group']);
-        let chatAvatar;
-
-        let chatMembers = [];
-        if (chatType === 'tat') {
-          chatAvatar = generateAvatar();
-
-          chatMembers.push(generateUser(idx, chatAvatar));
-        } else {
-          chatAvatar = '';
-
-          const chatMembersCount = faker.datatype.number({ min: 1, max: 8 });
-
-          // eslint-disable-next-line arrow-body-style
-          chatMembers = Array.from({ length: chatMembersCount }, (elem, jdx) => generateUser(jdx, generateAvatar()));
-        }
-
-        const chatTitle = chatType === 'tat' ? chatMembers[0].username : faker.name.title();
-
-        return {
-          id: String(idx),
-          avatarSrc: chatAvatar,
-          title: chatTitle,
-          type: chatType,
-          members: chatMembers,
-        };
-      });
-    }
-
     return {
       chats,
     };
@@ -71,6 +22,52 @@ export const useChatsStore = defineStore('chatsStore', {
       if (!config.isUseMockedData) {
         const chatsData = await chatsService.getChats();
         this.chats = chatsData.result;
+      } else {
+        // eslint-disable-next-line arrow-body-style
+        const generateAvatar = () => {
+          return faker.random.arrayElement([faker.image.avatar(), '']);
+        };
+
+        // eslint-disable-next-line arrow-body-style
+        const generateUser = (index, avatar) => {
+          return {
+            id: String(index),
+            avatarSrc: avatar,
+            username: faker.name.firstName(),
+            university: faker.random.arrayElement(['eaf', 'pt', 'pht', 'ns', 'pac', 'wwe', 'care', 'hwwcast', 'mait', 'r']),
+            type: faker.random.arrayElement(['student', 'teacher']),
+          };
+        };
+
+        // eslint-disable-next-line arrow-body-style
+        this.chats = Array.from({ length: 12 }, (el, idx) => {
+          const chatType = faker.random.arrayElement(['tat', 'group']);
+          let chatAvatar;
+
+          let chatMembers = [];
+          if (chatType === 'tat') {
+            chatAvatar = generateAvatar();
+
+            chatMembers.push(generateUser(idx, chatAvatar));
+          } else {
+            chatAvatar = '';
+
+            const chatMembersCount = faker.datatype.number({ min: 1, max: 8 });
+
+            // eslint-disable-next-line arrow-body-style
+            chatMembers = Array.from({ length: chatMembersCount }, (elem, jdx) => generateUser(jdx, generateAvatar()));
+          }
+
+          const chatTitle = chatType === 'tat' ? chatMembers[0].username : faker.name.title();
+
+          return {
+            id: String(idx),
+            avatarSrc: chatAvatar,
+            title: chatTitle,
+            type: chatType,
+            members: chatMembers,
+          };
+        });
       }
     },
   },
