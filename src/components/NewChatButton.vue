678e684a6fdc373f7e0dd0afd2431b2da35e299d
int1m <maxmonsta@gmail.com>
2022-03-09 20:40:30 +0300

    fix(NewChatButton): add animation

--- src/components/NewChatButton.vue
+++ src/components/NewChatButton.vue
@@ -10,11 +10,24 @@
           @click="onClickHandler"
         >
           <template #icon>
-            <v-icon
-              name="clear"
-              :src="PenIcon"
-              fill="white"
-            />
+            <Transition name="pen-icon">
+              <v-icon
+                v-if="!isShow"
+                class="new-chat-button-icon"
+                name="clear"
+                :src="PenIcon"
+                fill="white"
+              />
+            </Transition>
+            <Transition name="close-icon">
+              <v-icon
+                v-if="isShow"
+                name="close"
+                :src="CloseIcon"
+                :size="32"
+                fill="white"
+              />
+            </Transition>
           </template>
         </v-button>
       </div>
@@ -51,6 +64,7 @@ import VButton from '@/components/kit/VButton.vue';
 import VIcon from '@/components/kit/VIcon.vue';
 
 import PenIcon from '@/assets/icons/pen.svg?url';
+import CloseIcon from '@/assets/icons/x.svg?url';
 
 const isShow = ref(false);
 
@@ -66,10 +80,51 @@ const onClickOutsideHandler = e => {
 </script>
 
 <style lang="scss" scoped>
-
 .new-chat-button-trigger {
   position: absolute;
   bottom: 1rem;
   right: 1rem;
+}
+
+.new-chat-button-icon {
+  position: absolute;
+}
+
+.pen-icon-enter-active {
+  animation: pen-icon-in .3s;
+}
+
+.pen-icon-leave-active {
+  animation: pen-icon-in .2s reverse;
+}
+
+@keyframes pen-icon-in {
+  0% {
+    opacity: 0;
+    transform: scale(0%);
+  }
+  100% {
+    opacity: 1;
+    transform: scale(100%);
+  }
+}
+
+.close-icon-enter-active {
+  animation: close-icon-in .3s;
+}
+
+.close-icon-leave-active {
+  animation: close-icon-in .2s reverse;
+}
+
+@keyframes close-icon-in {
+  0% {
+    opacity: 0;
+    transform: scale(0%);
+  }
+  100% {
+    opacity: 1;
+    transform: scale(100%);
+  }
 }
 </style>
