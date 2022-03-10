2b3193721de7f77c42e7a64a6d93edde12824908
int1m <maxmonsta@gmail.com>
2022-03-10 14:59:24 +0300

    feat(vite.config.js): output dir path

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,14 @@ import Vue from '@vitejs/plugin-vue';
 export default defineConfig({
   root: path.resolve(__dirname, 'src'),
   publicDir: path.resolve(__dirname, 'public'),
+  build: {
+    outDir: path.resolve(__dirname, 'dist'),
+    rollupOptions: {
+      input: {
+        index: path.resolve(__dirname, 'src/index.html'),
+      },
+    },
+  },
   resolve: {
     alias: {
       '@/': `${path.resolve(__dirname, 'src')}/`,
