2fc2e0b78af5e77f9d17a5137d87bbfb93b1069b
int1m <maxmonsta@gmail.com>
2022-03-10 14:45:04 +0300

    refactoring(index.html): move to src; fix(index.html): user-scale

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,16 @@ import { defineConfig } from 'vite';
 import Vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+  root: path.resolve(__dirname, 'src'),
+  publicDir: path.resolve(__dirname, 'public'),
+  build: {
+    outDir: path.resolve(__dirname, 'dist'),
+    rollupOptions: {
+      input: {
+        index: path.resolve(__dirname, 'public/index.html'),
+      },
+    },
+  },
   resolve: {
     alias: {
       '@/': `${path.resolve(__dirname, 'src')}/`,
