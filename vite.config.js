e5ffbbfafceb59c85d498414f61252d3bcf0663f
int1m <maxmonsta@gmail.com>
2022-03-11 20:12:28 +0300

    feat: init PWA

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@ import path from 'path';
 import { defineConfig } from 'vite';
 import Vue from '@vitejs/plugin-vue';
 
+import { VitePWA } from 'vite-plugin-pwa';
+
 export default defineConfig({
   root: path.resolve(__dirname, 'src'),
   publicDir: path.resolve(__dirname, 'public'),
@@ -19,6 +21,33 @@ export default defineConfig({
     },
   },
   plugins: [
+    VitePWA({
+      includeAssets: ['favicon.svg', 'favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
+      manifest: {
+        name: 'Name of your app',
+        short_name: 'Short name of your app',
+        description: 'Description of your app',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
+      },
+    }),
     Vue({
       include: [/\.vue$/],
     }),
