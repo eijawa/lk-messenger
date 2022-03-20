bf39101063f47c1d6d063c9b2a87c1932de39a48
int1m <maxmonsta@gmail.com>
2022-03-20 19:11:00 +0300

    feat(NewChatButton): add v-menu-button

--- src/components/NewChatButton.vue
+++ src/components/NewChatButton.vue
@@ -36,26 +36,29 @@
         </v-button>
       </div>
     </template>
-    <div>
-      Теееееееееееееекст
-    </div>
-    <div>
-      Теееееееееееееекст
-    </div>
-    <div>
-      Теееееееееееееекст
-    </div>
-    <div>
-      Теееееееееееееекст
-    </div>
-    <div>
-      Теееееееееееееекст
-    </div>
-    <div>
-      Тееееееееееееееееееееееееееееееееееееекст
-    </div>
-    <div>
-      Теееееееееееееекст
+    <div class="new-chat-button-menu">
+      <v-menu-button size="small" :font-size="0.875">
+        <template #icon>
+          <v-icon
+            :src="GroupIcon"
+            name="group"
+            :size="20"
+            :fill="iconColor"
+          />
+        </template>
+        Создать группу
+      </v-menu-button>
+      <v-menu-button size="small" :font-size="0.875">
+        <template #icon>
+          <v-icon
+            :src="UserIcon"
+            name="user"
+            :size="20"
+            :fill="iconColor"
+          />
+        </template>
+        Создать чат
+      </v-menu-button>
     </div>
   </v-popover>
 </template>
@@ -63,14 +66,20 @@
 <script setup>
 import { ref } from 'vue';
 
+import { getCSSVariable } from '@/helpers/cssVariablesHelper';
 import VPopover from '@/components/kit/VPopover.vue';
 import VButton from '@/components/kit/VButton.vue';
+import VMenuButton from '@/components/kit/VMenuButton.vue';
 import VIcon from '@/components/kit/VIcon.vue';
 
 import PenIcon from '@/assets/icons/pen.svg?url';
 import CloseIcon from '@/assets/icons/x.svg?url';
+import UserIcon from '@/assets/icons/user.svg?url';
+import GroupIcon from '@/assets/icons/group.svg?url';
 
 const isShow = ref(false);
+
+const iconColor = getCSSVariable('--color-icon-secondary');
 
 const onClickHandler = () => {
   isShow.value = !isShow.value;
@@ -88,6 +97,13 @@ const onClickOutsideHandler = e => {
   position: absolute;
   bottom: 1rem;
   right: 1rem;
+}
+
+.new-chat-button-menu {
+  width: 200px;
+  padding: 0.25rem 0;
+  display: flex;
+  flex-direction: column;
 }
 
 .new-chat-button-icon {
