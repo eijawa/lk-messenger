307594fac6552e434a3b797fd39e3be8f47e59db
int1m <maxmonsta@gmail.com>
2022-03-22 15:50:59 +0300

    refactoring(messengerSettingsStore): rewrite on TS

--- src/stores/messengerSettingsStore.ts
+++ src/stores/messengerSettingsStore.ts
@@ -2,7 +2,7 @@ import { defineStore } from 'pinia';
 import { ref } from 'vue';
 
 export const useMessengerSettingsStore = defineStore('messengerSettingsStore', () => {
-  const isChatOpened = ref(false);
+  const isChatOpened = ref<boolean>(false);
 
   return {
     isChatOpened,
