56952e2ae2d3dc22345cffc9a998b0dfff3d3f1c
int1m <maxmonsta@gmail.com>
2022-03-22 09:49:23 +0300

    refactoring(useStringFormatter): rewrite on TS

--- src/use/useStringFormatter.ts
+++ src/use/useStringFormatter.ts
@@ -1,13 +1,11 @@
-/*
+/**
   Get first (max) 3 letters of title.
   If letters > 3, then return first letter of first and last words.
 */
-const getFirstLetters = s => {
-  let sValue = s.split(' ').map(x => x[0]);
+export const getFirstLetters = (name: string) => {
+  let sValue = name.split(' ').map(x => x[0]);
   if (sValue.length > 3) {
     sValue = [sValue[0], sValue[sValue.length - 1]];
   }
   return sValue.join('');
 };
-
-export default getFirstLetters;
