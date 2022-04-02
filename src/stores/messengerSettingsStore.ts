0446f4f2f937804a9708f2747673c4d726e8fc7e
int1m <maxmonsta@gmail.com>
2022-04-02 17:40:57 +0300

    fix(messengerSettingsStore): remove trash

--- src/stores/messengerSettingsStore.ts
+++ src/stores/messengerSettingsStore.ts
@@ -1,43 +1,12 @@
 import { defineStore } from 'pinia';
 import {
-  computed, DefineComponent, ref, shallowRef,
+  ref,
 } from 'vue';
-import { LeftColumnComponentsList, LeftColumnComponent } from '@/types/MessengerSettings';
-import Sidebar from '@/views/messenger/SidebarView.vue';
 
 export const useMessengerSettingsStore = defineStore('messengerSettingsStore', () => {
-  const leftColumnActiveComponent = shallowRef(Sidebar);
-  const leftColumnActiveComponentGetter = computed(() => leftColumnActiveComponent.value);
-  const leftColumnComponentsList: LeftColumnComponentsList = [
-    {
-      name: 'Sidebar',
-      path: 'components/SidebarView',
-    },
-    {
-      name: 'Settings',
-      path: 'views/leftColumn/settings/SettingsView',
-    },
-  ];
-
-  const leftColumnActiveComponentChange = async (componentName: string) => {
-    const searchedComponentInfo = leftColumnComponentsList.find(
-      (component: LeftColumnComponent) => component.name === componentName,
-    );
-    if (typeof searchedComponentInfo !== 'undefined') {
-      leftColumnActiveComponent.value = (
-        await import(`../${searchedComponentInfo.path}.vue`) as DefineComponent
-      ).default as DefineComponent;
-    } else {
-      throw new Error(`component "${componentName}" not found`);
-    }
-  };
-
   const isChatOpened = ref<boolean>(false);
 
   return {
-    leftColumnActiveComponent,
-    leftColumnActiveComponentGetter,
-    leftColumnActiveComponentChange,
     isChatOpened,
   };
 });
