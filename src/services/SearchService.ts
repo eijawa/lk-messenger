29d2f835b1f3e24a86724e36cfba5a7cc27f6337
int1m <maxmonsta@gmail.com>
2022-04-06 16:37:36 +0300

    feat(SearchService): init

--- src/services/SearchService.ts
+++ src/services/SearchService.ts
@@ -1,12 +1,24 @@
 import { useFetch } from '@/hooks/useService';
 import config from '@/config';
 
+interface ChatOrContact {
+  id: number,
+  username: string,
+  avatar: string | undefined,
+  verified: boolean,
+  label: string,
+}
+
+interface Search {
+  ChatsAndContacts: Array<ChatOrContact>,
+}
+
 export class SearchService {
-  messengerSearch = async (query: string) => {
+  messengerSearch = async (query: string): Promise<Search> => {
     const url = new URL(`${config.baseUrl}/find`);
     url.search = new URLSearchParams({ text: query }).toString();
 
-    const response = await useFetch(url.toString(), {
+    const response = await useFetch<Search>(url.toString(), {
       method: 'GET',
     });
 
