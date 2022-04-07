0e4b9baa51af1cfa4f0dcb8374504af778046476
int1m <maxmonsta@gmail.com>
2022-04-07 11:42:08 +0300

    feat(VInlineSvg): dynamic import

--- src/lib/v-inline-svg/VInlineSvg.vue
+++ src/lib/v-inline-svg/VInlineSvg.vue
@@ -1,25 +1,64 @@
-<script lang="ts" setup>
-import { onMounted, defineProps } from 'vue';
-import { downloadSvg } from '@/lib/v-inline-svg/VInlineSvgServices';
+<script lang="ts">
+import { onMounted, ref, h } from 'vue';
 
-const props = defineProps({
-  src: {
-    type: String,
-    required: true,
-  },
-  title: {
-    type: String,
-    required: true,
-  },
-});
+interface Props {
+  src: string;
+  title: string;
+  height: number,
+  width: number,
+}
 
-onMounted(async () => {
-  await downloadSvg(props.src);
-});
+export default {
+  props: {
+    src: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    height: {
+      type: Number,
+      default: () => 24,
+    },
+    width: {
+      type: Number,
+      default: () => 24,
+    },
+  },
+  setup(props: Props) {
+    const svgInnerHTML = ref('');
+    const viewBox = ref<string>(`0 0 ${props.width} ${props.height}`);
+
+    const importSvgContent = async () => {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+      const svgRaw = (await import(`${props.src}?raw`)).default as string;
+      const parser = new DOMParser();
+      const svgDocument = parser.parseFromString(svgRaw, 'text/html');
+      const svgNode = svgDocument.querySelector('svg');
+      if (svgNode) {
+        svgInnerHTML.value = svgNode.innerHTML;
+      }
+    };
+
+    onMounted(async () => {
+      await importSvgContent();
+    });
+
+    return () => h('i', {
+      style: {
+        height: `${props.height}px`,
+        width: `${props.width}px`,
+      },
+    }, [
+      h('svg', {
+        viewBox: viewBox.value,
+        height: `${props.height}px`,
+        width: `${props.width}px`,
+        innerHTML: svgInnerHTML.value,
+      }),
+    ]);
+  },
+};
 </script>
-
-<template>
-  <div>
-    123
-  </div>
-</template>
